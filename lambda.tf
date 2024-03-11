9fd0b54a3b764b951f11e0cdda5e654aa0475a9f
Cristobal Grau <cristobalgrau@gmail.com>
2024-03-11 02:15:42 +0000

    #5 fixed rds private connection with lambda and created the rds public

--- lambda.tf
+++ lambda.tf
@@ -1,5 +1,7 @@
 # ==== LAMBDA SECTION ====
 
+# ----> LAMBDA SECTION FOR PRIVATE RDS <----
+
 # Created Lambda function for Private RDS
 resource "aws_lambda_function" "private-lambda" {
   filename      = "${path.module}/lambda/lambda_function_payload.zip"
@@ -12,22 +14,41 @@ resource "aws_lambda_function" "private-lambda" {
   runtime = "python3.9"
 
   # ENV VARS for Lambda
-    environment {
-      variables = {
-        RDS_ENDPOINT = aws_db_instance.rds-private.address
-        DB_USER = var.db-username
-        DB_PASSWORD = var.db_pass
-        DB_NAME = var.db-name
-      }
+  environment {
+    variables = {
+      RDS_ENDPOINT = aws_db_instance.rds-private.address
+      DB_USER      = var.db-username
+      DB_PASSWORD  = var.db_pass
+      DB_NAME      = var.db-name
     }
+  }
 
   vpc_config {
     subnet_ids         = [for subnet_key, subnet_value in aws_subnet.private_subnets : subnet_value.id]
-    security_group_ids = [aws_default_security_group.default.id]
+    security_group_ids = [aws_default_security_group.default.id, aws_security_group.allow-rds-to-lambda.id]
   }
 
   layers = [aws_lambda_layer_version.lambda_layer.arn]
 }
+
+# Create Security Group to allow Cconnection between Lamda and RDS
+resource "aws_security_group" "allow-rds-to-lambda" {
+  name        = "lambda-rds-connect"
+  description = "Allow outbound traffic from lambda to RDS"
+  vpc_id      = aws_vpc.vpc.id
+}
+
+# Created ingress rule for all traffic from Lambda to RDS
+resource "aws_vpc_security_group_egress_rule" "allow-rds-to-lambda" {
+  security_group_id            = aws_security_group.allow-rds-to-lambda.id
+  ip_protocol                  = "tcp"
+  from_port                    = 3306
+  to_port                      = 3306
+  referenced_security_group_id = aws_security_group.allow-lambda-to-rds.id
+  #security_groups = [aws_security_group.allow-lambda-to-rds]
+}
+
+# ----> LAMBDA SECTION FOR PUBLIC RDS <----
 
 # Created Lambda function for Public RDS
 resource "aws_lambda_function" "public-lambda" {
@@ -41,14 +62,14 @@ resource "aws_lambda_function" "public-lambda" {
   runtime = "python3.9"
 
   # ENV VARS for Lambda
-  #   environment {
-  #     variables = {
-  #       RDS_ENDPOINT = 
-  #       DB_USER = 
-  #       DB_PASSWORD = 
-  #       DB_NAME = 
-  #     }
-  #   }
+  environment {
+    variables = {
+      RDS_ENDPOINT = aws_db_instance.rds-public.address
+      DB_USER      = var.db-username
+      DB_PASSWORD  = var.db_pass
+      DB_NAME      = var.db-name
+    }
+  }
 
   layers = [aws_lambda_layer_version.lambda_layer.arn]
 }
--- rds.tf
+++ rds.tf
@@ -22,14 +22,45 @@ resource "aws_db_instance" "rds-private" {
   engine_version          = "8.0.35"
   instance_class          = "db.t3.micro"
   username                = var.db-username
-  password                = var.db_pass   #hot to use ENV VARS from shell: https://support.hashicorp.com/hc/en-us/articles/4547786359571-Reading-and-using-environment-variables-in-Terraform-runs
+  password                = var.db_pass #hot to use ENV VARS from shell: https://support.hashicorp.com/hc/en-us/articles/4547786359571-Reading-and-using-environment-variables-in-Terraform-runs
   parameter_group_name    = "default.mysql8.0"
   skip_final_snapshot     = true
   backup_retention_period = 0
   db_subnet_group_name    = aws_db_subnet_group.private-db-subnet.name
+  vpc_security_group_ids  = [aws_security_group.allow-lambda-to-rds.id]
 }
 
-# resource "aws_db_instance" "rds-private" {
-#   db_name = "rds-db-private"
-#   instance_class = "db.t3.micro"
-# }
+# Create Security Group to allow traffic from Lamda to RDS
+resource "aws_security_group" "allow-lambda-to-rds" {
+  name        = "rds-lambda-connect"
+  description = "Allow inbound traffic from lambda"
+  vpc_id      = aws_vpc.vpc.id
+}
+
+# Created ingress rule for all traffic from Lambda to RDS
+resource "aws_vpc_security_group_ingress_rule" "allow-lambda-to-rds" {
+  security_group_id = aws_security_group.allow-lambda-to-rds.id
+  # cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol                  = "tcp"
+  from_port                    = 3306
+  to_port                      = 3306
+  referenced_security_group_id = aws_security_group.allow-rds-to-lambda.id
+  # security_groups = [aws_security_group.allow-rds-to-lambda]
+}
+
+# Create RDS Public
+resource "aws_db_instance" "rds-public" {
+  allocated_storage       = 20
+  db_name                 = var.db-name
+  identifier              = var.rds-public-db-name
+  engine                  = "mysql"
+  engine_version          = "8.0.35"
+  instance_class          = "db.t3.micro"
+  username                = var.db-username
+  password                = var.db_pass #hot to use ENV VARS from shell: https://support.hashicorp.com/hc/en-us/articles/4547786359571-Reading-and-using-environment-variables-in-Terraform-runs
+  parameter_group_name    = "default.mysql8.0"
+  skip_final_snapshot     = true
+  backup_retention_period = 0
+  db_subnet_group_name    = aws_db_subnet_group.private-db-subnet.name
+  vpc_security_group_ids  = [aws_security_group.allow-all-traffic.id]
+}
